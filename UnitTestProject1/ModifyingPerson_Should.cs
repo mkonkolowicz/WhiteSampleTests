c253f158d8a4f3445c7db9ec84789d14e005a196
mkonkolowicz <mkonkolowicz@gmail.com>
2016-02-10 23:26:38 -0500

    Sample app commit and initial test commit

---
+++ UnitTestProject1/ModifyingPerson_Should.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using TestStack.White;
+using TestStack.White.UIItems.WindowItems;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace WhiteSampleTests
+{
+    [TestFixture]
+    public class ModifyingPerson_Should
+    {
+           
+        [Test]
+        public void ChangeUserLastName()
+        {
+            List<Window> windowList = Desktop.Instance.Windows();
+
+            Window windowUnderTest = windowList.SingleOrDefault(x => x.Id.Contains("MainWindow"));
+
+            windowUnderTest.DrawHighlight();
+        }
+    }
+    //ToDo: Add page object pattern via factory
+}
